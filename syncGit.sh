55a7539516eea0b2f96775f387322c3dfc57961a
vandelvan <vandel_van@hotmail.com>
2020-02-20 19:53:27 -0600

    Nuevo Cambio jue feb 20 19:53:27 CST 2020

--- syncGit.sh
+++ syncGit.sh
@@ -16,6 +16,7 @@ if [ -z "$clean" ]; then
     git add .
     git commit -m "Nuevo Cambio $(date)"
     git push -vvv git@github.com:vandelvan/semArq.git master
+    ivanorozco
 else
     echo Branch is clean.
 fi
