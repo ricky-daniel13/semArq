fa3251eda53522fbb94457cf689ffae1b780ec18
vandelvan <vandel_van@hotmail.com>
2020-02-20 19:52:01 -0600

    Nuevo Cambio jue feb 20 19:52:01 CST 2020

--- syncGit.sh
+++ syncGit.sh
@@ -1,6 +1,14 @@
 #!/bin/sh    
 #SINCRONIZA AUTOMATICAMENTE LOS PROYECTOS DE SEMINARIO DE ARQUITECTURA A GITHUB
 #Orozco Torrez José Iván
+itest=$(fping google.com | grep alive)
+
+while [ "$itest" == "" ] 
+    do
+    sleep 5
+    itest=$(fping google.com | grep alive)
+done
+echo Conectado!
 cd ~/Documentos/semArq
 clean=$(git status | grep "nothing to commit (working directory clean)")
 if [ -z "$clean" ]; then
