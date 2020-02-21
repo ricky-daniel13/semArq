d1908ea2787f3e8e5508e8fb6c579ac5d49231d7
vandelvan <vandel_van@hotmail.com>
2020-02-20 20:08:06 -0600

    Nuevo Cambio jue feb 20 20:08:06 CST 2020

--- syncGit.sh
+++ syncGit.sh
@@ -1,21 +1,21 @@
 #!/bin/sh    
 #SINCRONIZA AUTOMATICAMENTE LOS PROYECTOS DE SEMINARIO DE ARQUITECTURA A GITHUB
 #Orozco Torrez José Iván
-itest=$(fping google.com | grep alive)
+itest=$(fping google.com | grep conectado)
 
 while [ "$itest" == "" ] 
     do
     sleep 5
-    itest=$(fping google.com | grep alive)
+    itest=$(fping google.com | grep conectado)
 done
-echo Conectado!
+echo Conectado
 cd ~/Documentos/semArq
-clean=$(git status | grep "nothing to commit (working directory clean)")
+clean=$(git status | grep "Sin cambios")
 if [ -z "$clean" ]; then
     git pull
     git add .
     git commit -m "Nuevo Cambio $(date)"
     git push -vvv git@github.com:vandelvan/semArq.git master
 else
-    echo Branch is clean.
+    echo Sin cambios.
 fi
