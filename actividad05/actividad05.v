bf792f8bacb0379879005307f30446a8097efca9
vandelvan <vandel_van@hotmail.com>
2020-02-24 15:29:41 -0600

    Nuevo Cambio lun feb 24 15:29:41 CST 2020

--- actividad05/actividad05.v
+++ actividad05/actividad05.v
@@ -5,63 +5,69 @@ module actividad05(
 	input enable,
 	output reg [31:0]salida
 );
-wire [5:0]c;
+wire [31:0]
+	c,
+	c1,
+	c2,
+	c3,
+	c4,
+	c5;
 compAnd cA(
 	.a(a),
 	.b(b),
 	.enable(enable),
-	.salida(c[0])
+	.salida(c)
 );
 
 compOr cO(
 	.a(a),
 	.b(b),
 	.enable(enable),
-	.salida(c[1])
+	.salida(c1)
 );
 
 compNand cN(
 	.a(a),
 	.b(b),
 	.enable(enable),
-	.salida(c[2])
+	.salida(c2)
 );
 
 suma s(
 	.a(a),
 	.b(b),
 	.enable(enable),
-	.salida(c[3])
+	.salida(c3)
 );
 
 resta r(
 	.a(a),
 	.b(b),
 	.enable(enable),
-	.salida(c[4])
+	.salida(c4)
 );
 
 multiplicacion m(
 	.a(a),
 	.b(b),
 	.enable(enable),
-	.salida(c[5])
+	.salida(c5)
 );
 always @*
 begin
 	case(sel)
 		3'b000:
-			salida = c[0];
+			salida = c;
 		3'b001:
-			salida = c[1];
+			salida = c1;
 		3'b100:
-			salida = c[2];
+			salida = c2;
 		3'b010:
-			salida = c[3];
+			salida = c3;
 		3'b011:
-			salida = c[4];
+			salida = c4;
 		3'b111:
-			salida = c[5];
+			salida = c5;
 		default:
 			salida = 0;
 	endcase
