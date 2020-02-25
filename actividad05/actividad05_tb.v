5d738fe6566452449cc42550b010d50b414f5c7d
vandelvan <vandel_van@hotmail.com>
2020-02-24 19:50:42 -0600

    Nuevo Cambio lun feb 24 19:50:42 CST 2020

--- actividad05/actividad05_tb.v
+++ actividad05/actividad05_tb.v
@@ -40,14 +40,14 @@ begin
 	en = 1;
 	#20;
 	//prueba 5 resta
-	a = 32'd8;
-	b = 32'd16;
+	a = 32'd6;
+	b = 32'd12;
 	sel = 3'b011;
 	en = 1;
 	#20;
 	//prueba 6 Multiplicacion
-	a = 32'd8;
-	b = 32'd16;
+	a = 32'd10;
+	b = 32'd24;
 	sel = 3'b111;
 	en = 1;
 	#20;
@@ -58,14 +58,14 @@ begin
 	en = 1;
 	#20;
 	//prueba 8 Seleccion invalida
-	a = 32'd8;
-	b = 32'd16;
+	a = 32'd84;
+	b = 32'd36;
 	sel = 3'b101;
 	en = 1;
 	#20;
 	//prueba 9 enable 0
-	a = 32'd8;
-	b = 32'd16;
+	a = 32'd28;
+	b = 32'd136;
 	sel = 3'b010;
 	en = 0;
 	#20;
@@ -77,33 +77,63 @@ begin
 	#20;
 	//prueba 11 OR
 	a = 32'd23141251;
-	b = 32'd6669;
+	b = 32'd35236234;
 	sel = 3'b001;
 	en = 1;
 	#20;
 	//prueba 12 NAND
 	a = 32'd23141251;
-	b = 32'd6669;
+	b = 32'd35236234;
 	sel = 3'b100;
 	en = 1;
 	#20;
 	//prueba 13 suma
-	a = 32'd8;
-	b = 32'd16;
+	a = 32'd2524;
+	b = 32'd1;
 	sel = 3'b010;
 	en = 1;
 	#20;
 	//prueba 14 resta
-	a = 32'd8;
-	b = 32'd16;
+	a = 32'd50;
+	b = 32'd5;
 	sel = 3'b011;
 	en = 1;
 	#20;
 	//prueba 15 Multiplicacion
-	a = 32'd8;
-	b = 32'd16;
+	a = 32'd4;
+	b = 32'd100;
 	sel = 3'b111;
 	en = 1;
+	#20;
+	//prueba 16 suma
+	a = 32'd222;
+	b = 32'd23;
+	sel = 3'b010;
+	en = 1;
+	#20;
+	//prueba 17 resta
+	a = 32'd2;
+	b = 32'd3;
+	sel = 3'b100;
+	en = 1;
+	#20;
+	//prueba 18 multiplicacion
+	a = 32'd2524;
+	b = 32'd1;
+	sel = 3'b111;
+	en = 1;
+	#20;
+	//prueba 19 resta
+	a = 32'd220;
+	b = 32'd220;
+	sel = 3'b011;
+	en = 1;
+	#20;
+	//prueba 20 enable 0
+	a = 32'd4;
+	b = 32'd100;
+	sel = 3'b111;
+	en = 0;
 	#20;
 end
 endmodule
