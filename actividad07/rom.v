fbdce725d2dcf447e439e82f368f729f5c94decf
vandelvan <vandel_van@hotmail.com>
2020-03-12 16:10:14 -0600

    Nuevo Cambio jue mar 12 16:10:14 CST 2020

--- actividad07/rom.v
+++ actividad07/rom.v
@@ -1,10 +1,12 @@
 module rom(
-	input [4:0]dir,
-	output reg [31:0]inst
+	input [6:0]dir,
+	output [31:0]inst
 );
-reg [7:0] isa [0:25];
-always @*
+reg [7:0] isa [0:127];
+assign inst = {isa[dir+3],isa[dir+2],isa[dir+1],isa[dir]};
+
+initial @*
 begin
-	
+	$readmemh("rom.mem", isa);
 end
 endmodule
